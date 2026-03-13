c4e0c92f660ce271f370efdbde4ab7f84d8bb12f
Anuska Ghosh <98759881+Anuska93@users.noreply.github.com>
2026-03-13 11:12:37 +0530

    Update pattern3.c

--- pattern3.c
+++ pattern3.c
@@ -27,3 +27,34 @@ Enter n: 10
        *****
         ***
          *
+
+    OR
+
+    // Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int i,j,k,n;
+    printf("Enter n: ");
+    scanf("%d",&n);
+    for(i=n;i>=0;i--){
+        for(j=0;j<=n-i;j++){
+            printf(" ");
+        }
+        for(k=0;k<2*i+1;k++){
+            printf("*");
+        }printf("\n");
+    }
+     return 0;
+}
+
+Enter n: 5
+ ***********
+  *********
+   *******
+    *****
+     ***
+      *
+
+
+=== Code Execution Successful ===
