9e692531e0386234a2a2d197f4766a3d00cedddb
Anuska Ghosh <98759881+Anuska93@users.noreply.github.com>
2026-03-17 10:35:33 +0530

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int fib(int n);
+int main() {
+    int f=fib(1);
+    printf("%d",f);
+    return 0;
+}
+int fib(int n){
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    return fib(n-1)+fib(n-2);
+}
+// 0 1 1 2 3 5 8
+// 3-> 2 1=2  2-> 1 0=1 
