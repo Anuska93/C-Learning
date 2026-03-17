eb6e8105634fbea8621ef588f43eca95cf248b56
Anuska Ghosh <98759881+Anuska93@users.noreply.github.com>
2026-03-17 10:59:56 +0530

    Update Recursion_Sum of N nos.c

--- Recursion_Sum of N nos.c
+++ Recursion_Sum of N nos.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+int sum(int n);
+int main(){
+     int s =sum(50);
+     printf("%d",s);
+     return 0;
+}
+int sum(int n){
+    if(n==0){
+        return 0;
+    }
+    return n+sum(n-1);
+}
+
+
 
