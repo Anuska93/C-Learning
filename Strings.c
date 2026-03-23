70a8c040b700a7a232bf6fe96234f5c35e7276ab
Anuska Ghosh <98759881+Anuska93@users.noreply.github.com>
2026-03-23 21:42:50 +0530

    Update Strings.c

--- Strings.c
+++ Strings.c
@@ -21,3 +21,25 @@ int main() {
     return 0;
 }
 
+2. Copying a string into another string.
+
+#include <stdio.h>
+int main() {
+    int i=0,count=0;
+    char s[100],a[100];
+    printf("Enter string\n");
+    while(1){
+        scanf("%c",&s[i]);
+        if(s[i]=='\n'){
+            s[i]='\0';
+            break;
+        }
+        i++;
+    }
+        for(i=0;s[i]!='\0';i++){
+            a[i]=s[i];
+            //printf("%c",a[i]);
+        }
+    printf("%s",a);
+    return 0;
+}
