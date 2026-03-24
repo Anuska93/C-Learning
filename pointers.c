138413c629df4bb2b7ef37770b91cf46b4e0595d
Anuska Ghosh <98759881+Anuska93@users.noreply.github.com>
2026-03-24 15:27:28 +0530

    Update pointers.c

--- pointers.c
+++ pointers.c
@@ -16,3 +16,30 @@ o/p:
 f72b0254
 f72b0254
 22
+
+2. Basics of pointers.
+
+#include <stdio.h>
+int main(){
+    int i=20,*pi,**ppi;
+    pi=&i;
+    ppi=&pi;
+    printf("Value of i is: %d ",i);
+    printf("\nAddress of i is: %x",&i);
+    printf("\nvalue of pi is: %x",pi);
+    printf("\nAddress of pi is: %x",&pi);
+    printf("\nValue of ppi is: %x",ppi);
+    printf("\nValue of i using pi is: %d",*pi);
+    printf("\nValue of i using ppi is: %d",**ppi);
+    printf("\nValue of pi using ppi is: %x",*ppi);
+    return 0;
+}
+
+Value of i is: 20 
+Address of i is: 47424f84
+value of pi is: 47424f84
+Address of pi is: 47424f78
+Value of ppi is: 47424f78
+Value of i using pi is: 20
+Value of i using ppi is: 20
+Value of pi using ppi is: 47424f84
