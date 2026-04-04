0cb87e4a841cc6c2ddc8766fa28c426d88d2de8d
Anuska Ghosh <98759881+Anuska93@users.noreply.github.com>
2026-04-04 21:08:55 +0530

    Update Palindrome.c

--- Palindrome.c
+++ Palindrome.c
@@ -28,3 +28,28 @@ int main(){
     }
     return 0;
 }
+
+/*Procedure Palindrome()
+display "Size of Array?"
+read n
+display "Enter a string"
+while(true) do
+    read s[i]
+    if(s[i]=='\n')
+        s[i]='\0'
+            break
+    end if
+    i++
+end while
+for i=0 to n/2 do
+    if(s[i]==s[n-1-i])
+        f=1
+end for
+if(f==1)
+    display "palindrome"
+else
+    display "Not a palindrome"
+end if
+return 0
+end procedure
+*/
